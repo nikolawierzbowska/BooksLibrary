4c679e5d5264e03019795e0fdf645aec4c26e43c
nikolawierzbowska <nikolawierzbowska21@gmail.com>
2024-09-23 10:40:12 +0200

    Database has been created -seven books

---
+++ src/main/resources/V1_1__schema.sql
@@ -0,0 +1,9 @@
+insert into books(title, author, year, country)
+    VALUES
+        ('Obcy', 'Albert Camus', 1942,'Francja'),
+        ('W poszukiwaniu straconego czasu', 'Marcel Proust', 1927,'Francja'),
+        ('Przeminęło z wiatrem', 'Margaret Mitchell', 1936,'USA'),
+        ('Wielki Gatsby', 'Francisa Scotta Fitzgeralda', 1925,'USA'),
+        ('Lalka', 'Bolesław Prus', 1890,'Polska'),
+        ('Noce i dnie', 'Maria Dąbrowska', 1931,'Polska'),
+        ('Rebelia', 'Davis Siobhan', 2022,'Anglia');
